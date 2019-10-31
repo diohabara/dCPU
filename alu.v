e79226bf6fb979f96458dce386bdd280dd53d655
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2019-10-31 16:33:03 +0900

    until BRANCH

--- alu.v
+++ alu.v
@@ -12,6 +12,7 @@ module alu(
     reg [31:0] rs1;
     reg [31:0] rs2;
     reg [31:0] rd;
+    reg isBranch;
 
     always @(*) begin
         rs1 <= op1;
@@ -21,30 +22,76 @@ module alu(
                 rd <= rs1 + rs2;
             `ALU_SUB:
                 rd <= rs1 + rs2;
-            `ALU_SLT:
-            `ALU_SLTU:
+            `ALU_SLT: begin
+                if (rs1 < rs2)
+                    rd <= 1;
+                else
+                    rd <= 0;
+                end
+            `ALU_SLTU: begin
+                if (rs1 < rs2)
+                    rd <= 1;
+                else
+                    rd <= 0;
+                end
             `ALU_XOR:
+                rd <= rs1 ^ rs2;
             `ALU_OR:
+                rd <= rs1 | rs2;
             `ALU_AND:
+                rd <= rs1 & rs2;
             `ALU_SLL:
+                rd <= rs1 <<< rs2;
             `ALU_SRL:
+                rd <= rs1 <<< rs2;
             `ALU_SRA:
+                rd <= rs1 << rs1;
             `ALU_LUI:
+                rd <= rs2;
             `ALU_JAL:
+                rd <= rs2 + 4;
+                isBranch <= `ENABLE;
             `ALU_JALR:
+                rd <= rs2 + 4;
+                isBranch <= `ENABLE;
             `ALU_BEQ:
+                if (rs1 == rs2)
+                    isBranch <= `ENABLE;
+                else
+                    isBranch <= `DISABLE;
             `ALU_BNE:
+                if (rs1 != rs2)
+                    isBranch <= `ENABLE;
+                else
+                    isBranch <= `DISABLE;
+            `ALU_BLT:
+                if (rs1 < rs2)
+                    isBranch <= `ENABLE;
+                else
+                    isBranch <= `DISABLE;
             `ALU_BGE:
+                if (rs1 >= rs2)
+                    isBranch <= `ENABLE;
+                else
+                    isBranch <= `DISABLE;
+            `ALU_BLTU:
+                if (rs1 < rs2)
+                    isBranch <= `ENABLE;
+                else
+                    isBranch <= `DISABLE;
             `ALU_BGEU:
-            `ALU_LB:
-            `ALU_LH:
-            `ALU_LW:
-            `ALU_LW:
-            `ALU_LBU:
-            `ALU_LHU:
+                if (rs1 >= rs2)
+                    isBranch <= `ENABLE;
+                else
+                    isBranch <= `DISABLE;
             `ALU_SB:
             `ALU_SH:
             `ALU_SW:
+            `ALU_LB:
+            `ALU_LH:
+            `ALU_LW:
+            `ALU_LBU:
+            `ALU_LHU:
             default:;
         endcase
     end
