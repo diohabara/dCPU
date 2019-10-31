73e14e867e11575cf51b810e1ddcef84b1cd6e8c
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2019-10-31 15:58:01 +0900

    start alu

---
+++ alu.v
@@ -0,0 +1,15 @@
+`include "define.vh"
+
+module alu(
+    input reg [5:0] code;
+    input reg [31:0] op1;
+    input reg [31:0] op2;
+    output wire [31:0]  result;
+    output wire br;
+    );
+
+    always @(*) begin
+    
+    end
+
+endmodule
