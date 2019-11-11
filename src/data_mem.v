8ca1f48eef556efa3fd59437cfe21f370c4e5cff
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2019-11-11 14:33:05 +0900

    typo

--- src/data_mem.v
+++ src/data_mem.v
@@ -1,13 +1,13 @@
 `include "define.vh"
 
-module data_mem(address, clk, writeEn, write_data, read_data);
+module data_mem(address, clk, wren, write_data, read_data);
         input [7:0] address;
-        input clk, writeEn;
+        input clk, wren;
         input [7:0] write_data;
         output [7:0] read_data;
         reg[7:0] data_memory [255:0];
     always @(posedge clk) begin
-        if (writeEn == 0)
+        if (wren == 0)
             data_memory[address] <= write_data;
     end
     assign read_data = data_memory[address];
--- src/ram.v
+++ src/ram.v
@@ -1,12 +1,12 @@
-module ram(clk, we, r_addr, r_data, w_addr, w_data);
-        input clk, we;
+module ram(clk, wren, r_addr, r_data, w_addr, w_data);
+        input clk, wren;
         input  [4:0] r_addr, w_addr;
         input  [31:0] w_data;
         output [31:0] r_data;
         reg [4:0] addr_reg;
         reg [31:0] mem [0:31];
         always @(posedge clk) begin
-            if(we) mem[w_addr] <= w_data; //書き込みのタイミングを同期
+            if (wren) mem[w_addr] <= w_data; //書き込みのタイミングを同期
             addr_reg <= r_addr;           //読み出しアドレスを同期
         end
         assign r_data = mem[addr_reg];
