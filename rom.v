63d9bf42614f2b61efded9d5f9df8604846726ac
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2019-11-11 13:29:17 +0900

    almost done

---
+++ rom.v
@@ -0,0 +1,11 @@
+module rom(clk, r_addr, r_data);
+        input clk;
+        input  [4:0] r_addr;
+        output [31:0] r_data;
+        reg [4:0] addr_reg;
+        reg [31:0] mem [0:31]
+        always @(posedge clk) begin
+            addr_reg <= r_addr;           //読み出しアドレスを同期
+        end
+        assign r_data = mem[addr_reg];
+endmodule
