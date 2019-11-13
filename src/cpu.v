ce4e9ee4419064a595d6b0a1d94bd5e76e68d08b
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2019-11-13 14:52:51 +0900

    refactor

--- src/cpu.v
+++ src/cpu.v
@@ -5,11 +5,10 @@ module cpu(
 
     wire [31:0] ins, rs1, rs2, alu_result, imm, reg_data;
     wire [5:0] alucode;
-    wire [4:0] pc, rs_addr1, rs_addr2, rd_addr;
+    wire [4:0] pc, rs_addr1, rs_addr2, rd_addr, r_addr_reg, w_addr_reg, br_addr;
+    wire [3:0] mask_buffer;
     wire [1:0] aluop1_type, aluop2_type;
     wire br_taken, wren, is_load, is_store, is_halt;
-    wire [4:0] r_addr_reg, w_addr_reg, br_addr;
-    wire [3:0] mask_buffer;
 
     function [3:0] store_mask;
         input alucode;
