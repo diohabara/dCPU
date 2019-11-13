38e389830fa08534aa8dc5aa7ee803d280209cf0
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2019-11-13 13:55:41 +0900

    update pc!

--- src/pc.v
+++ src/pc.v
@@ -2,13 +2,15 @@ module pc (
     input clk,
     input rst_n,
     input br_taken,
-    input [31:0] result,
+    input [31:0] alu_result,
     output reg [4:0] pc
     );
 
     always @(posedge clk) begin
         if (rst_n == `DISABLE)
             pc <= 0;
+        else if (br_taken == `ENABLE)
+            pc <= pc + alu_result;
         else
             pc <= pc + 4;
     end
